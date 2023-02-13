0599527ac93c07bddb00292882d822205b951faf
naffiziqbal <f.nafiziqbal@gmail.com>
2023-02-13 22:10:58 +0600

    Initializing Project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1E50FF",
+          secondary: "#FFB830",
+          accent: "#0F182E",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
